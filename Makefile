dc5e4e499ab54314145eb97fccf968a5b0cf877a
Thomas Jarrand <thomas.jarrand@gmail.com>
2021-08-01 12:44:34 +0200

    Image optimization

--- Makefile
+++ Makefile
@@ -53,23 +53,6 @@ install@production:
 var/games:
 	cd var && git clone git@tom32i.fr:/home/git/gameoscope-screenshot.git games
 
-##########
-# Warmup #
-##########
-
-warmup:
-	# Generate thumbnails
-
-# Note: This task is invoked after a deployment to staging
-warmup@staging: export APP_ENV = prod
-warmup@staging:
-	# Generate thumbnails
-
-# Note: This task is invoked after a deployment to production
-warmup@production: export APP_ENV = prod
-warmup@production:
-	# Generate thumbnails
-
 #######
 # Run #
 #######
@@ -91,7 +74,7 @@ watch:
 	npx encore dev --watch
 
 ## Build application
-build: build-assets build-content optimize
+build: build-assets cache build-content optimize
 
 build-assets:
 	npx encore production
@@ -107,17 +90,13 @@ build@production: build
 optimize:
 	node optimize.js build
 
-thumbnail:
-	bin/console thumbnail:generate
+cache: export APP_ENV = prod
+cache:
+	bin/console showcase:cache-generate
 
-thumbnail@production: export APP_ENV = prod
-thumbnail@production: thumbnail
-
-clear-thumbnail:
-	bin/console thumbnail:clear
-
-clear-thumbnail@production: export APP_ENV = prod
-clear-thumbnail@production: clear-thumbnail
+clear-cache: export APP_ENV = prod
+clear-cache:
+	bin/console showcase:cache-clear
 
 ############
 # Security #
--- optimize.js
+++ optimize.js
@@ -3,40 +3,63 @@ const glob = require('glob');
 
 const [path] = process.argv.slice(2);
 
-function files(input, callback) {
-  const { src, ...options } = input;
+function optimizeAll(files, options = { quality: 80 }) {
+  const { length } = files;
+  let cores = sharp.concurrency();
 
-  return glob(src, options, (error, files) => files.forEach(callback));
+  console.info(`Optimizing ${length} files with ${cores} cores...`);
+
+  sharp.cache({ memory: 1000 });
+
+  const next = () => optimize(files.shift(), options, file => {
+    if (files.length) {
+      const treated = length - files.length;
+
+      process.stdout.clearLine();
+      process.stdout.cursorTo(0);
+      process.stdout.write(`File ${treated}/${length}: ${(treated/length * 100).toFixed(2)}%  -  ${file}`);
+
+      next();
+    } else {
+      cores--;
+
+      if (cores === 1) {
+        process.stdout.clearLine();
+        process.stdout.cursorTo(0);
+        console.info('Done!');
+      }
+    }
+  });
+
+  for (let i = 0; i < cores; i++) {
+    next();
+  }
 }
 
-function optimize(file, options = {}) {
-  const { resize, ...params } = options;
-  const ext = file.split('.').pop();
-  let img = sharp(file);
+function optimize(file, options = {}, callback = undefined) {
+  const write = (error, buffer) => sharp(buffer).toFile(file, () => callback(file));
 
-  if (resize) {
-    img = img.resize(...resize);
-  }
-
-  switch (ext) {
+  switch (file.split('.').pop()) {
     case 'jpeg':
     case 'jpg':
-      img = img.jpeg({ ...params });
-      break;
+      return sharp(file).jpeg(options).toBuffer(write);
 
     case 'png':
-      img = img.png({ compressionLevel: 9, ...params });
-      break;
+      return sharp(file).png({ compressionLevel: 9, ...options }).toBuffer(write);
 
     case 'gif':
-      img = img.gif({ ...params });
-      break;
+      return sharp(file).gif(options).toBuffer(write);
+
+    case 'webp':
+      return sharp(file).webp({ /*lossless: true, reductionEffort: 6,*/ ...options }).toBuffer(write);
 
     default:
       throw new Error(`Unsupported image type "${file}".`);
   }
-
-  return img.toBuffer().then(buffer => sharp(buffer).toFile(file));
 }
 
-files({ src: `${path}/**/*.@(png|jpg|jpeg|gif)`, ignore: `${path}/download/**/*.@(png|jpg|jpeg|gif)` }, optimize);
+glob(
+  `${path}/**/*.@(png|jpg|jpeg|gif)`, //|webp
+  { ignore: `${path}/download/**/*` },
+  (error, files) => optimizeAll(files)
+);
